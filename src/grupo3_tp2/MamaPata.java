510670f98aee4f66411099ca41fa0b1e802c451d
Francisco Minguez <franciscoandresminguez@gmail.com>
2025-08-28 19:56:28 -0300

    cambios en mamapata
    as

--- src/grupo3_tp2/MamaPata.java
+++ src/grupo3_tp2/MamaPata.java
@@ -12,7 +12,16 @@ import java.util.Scanner;
  * @author Adrian
  */
 public interface MamaPata {
+    int patas=2;
+    boolean tiene_Pico = true;
     
     void tocarOrgano(Scanner sc);
+    void tocarOrgano();
+    void graznar();
+    void caminar();
+    
+    static void descripcion() {
+    System.out.println("Una mamá pata tiene pico y " + patas + " patas.");
+}
     
 }
